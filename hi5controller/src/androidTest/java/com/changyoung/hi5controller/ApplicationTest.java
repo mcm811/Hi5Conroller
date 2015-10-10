320ba679483d5ce374af4eab9f569ccaca02abbc
Changmin Moon <changmin811@gmail.com>
2015-10-10 12:08:14 +0900

    Initial commit

---
+++ hi5controller/src/androidTest/java/com/changyoung/hi5controller/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.changyoung.hi5controller;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
